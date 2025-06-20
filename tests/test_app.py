14a54181d01f28aba23dd460ae2031ee6272bb82
The Pivi <peter.pivarcsik@google.com>
2025-06-20 15:52:59 +0200

    Adding another positive testcase

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,4 +4,5 @@ from proto_chat.app import greet # Itt importáljuk az új package névről
 def test_greet_function():
     assert greet("World") == "Hello, World!"
     assert greet("Jenkins") == "Hello, Jenkins!"
+    assert greet("User") == "Hello, User!"
     assert greet("User") != "Goodbye, User!" # Egy negatív teszt
