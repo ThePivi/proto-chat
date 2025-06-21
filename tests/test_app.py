3bd6b320413bec68c980ba91d8819833bef58f13
The Pivi <peter.pivarcsik@google.com>
2025-06-22 00:42:56 +0200

    Fixing Flake8 code quality problems

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,8 @@
 from proto_chat.app import greet
 
-def test_greet_function():
 
+def test_greet_function():
     assert greet("World") == "Hello, World!"
     assert greet("Jenkins") == "Hello, Jenkins!"
     assert greet("User") == "Hello, User!"
-    assert greet("User") != "Goodbye, User!"  # Egy negatív teszt
+    assert greet("User") != "Goodbye, User!"
